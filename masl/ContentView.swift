7bbfc8fe391a405863742b4ebc2b91bbcc491d4c
Evgeny Kuklin <ekuklin@boardmaps.com>
2020-02-06 17:56:18 +0300

    Initial Commit

---
+++ masl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  masl
+//
+//  Created by Evgeny Kuklin on 06/02/2020.
+//  Copyright © 2020 Evgeny Kuklin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
